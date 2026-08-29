db78c65a1b1b3ad9a16b9696d6b2025d11ec9576
79mas <petravicius.tomas@gmail.com>
2026-08-29 09:43:47 +0300

    Delete app directory

--- app/main.tsx
+++
@@ -1,8 +0,0 @@
-import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
-import KairoApp from "@/components/kairo/app";
-import "./globals.css";
-
-const root=document.getElementById("root");
-if(!root)throw new Error("Nerastas Kairo Ride langas.");
-createRoot(root).render(<StrictMode><KairoApp/></StrictMode>);
